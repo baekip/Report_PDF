c7c56a99d35b3241d6eda50833adf1375891e0a9
Inpyo Baek <inpyo.baek@therabio.kr>
2016-11-30 10:39:54 +0900

    WGS upaate

--- Variant_Resource/wrapper_report_wes.pl
+++ Variant_Resource/wrapper_report_wes.pl
@@ -28,7 +28,7 @@ my $project_path = $info{project_path};
 my $out_path = "$project_path/report";
 my $project_id = $info{project_id};
 my $dev_path = $info{dev_path};
-my $Rscript = "Rscript";
+my $Rscript = "/BiO/BioTools/miniconda2/bin/Rscript";
 my $java = "/BiO/BioTools/java/jre1.7.0_51/bin/java";
 checkFile( $java );
 my $text2pdf = "$dev_path/etc/text2pdf.jar"; 
