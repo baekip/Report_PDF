95cb5a542882a32a988679d091b129a48621d8a5
baekip <inpyo.baekip@theragenetex.com>
2016-12-30 09:24:04 +0900

    update WGS Report

--- WGS_Resource_Human/wrapper_report_wes.pl
+++ WGS_Resource_Human/wrapper_report_wes.pl
@@ -29,7 +29,7 @@ my $out_path = "$project_path/report";
 my $project_id = $info{project_id};
 my $dev_path = $info{dev_path};
 my $Rscript = "Rscript";
-my $java = "/BiO/BioTools/java/jre1.7.0_51/bin/java";
+my $java = $info{java_1_7}; 
 checkFile( $java );
 my $text2pdf = "$dev_path/etc/text2pdf.jar"; 
 checkFile ( $text2pdf );
