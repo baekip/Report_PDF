4e23e4a18026e011aa2ab78feb23efc57b91e609
Baekip <silverpoke@daum.net>
2018-03-02 16:07:12 +0900

    update clinical exome V2

---
+++ etc/Kit_type_sh/Clinical_Exome_V2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo [[11],[],[TL],[280]] > $2 
+echo Reference\	hg19 >> $2
+echo Library Prep Kit\	Clinical Research Exome V2 >> $2
+echo Read length\	$1 x 2 >> $2
+echo Target size\	67Mb >> $2
+echo Number of probes\	843,912 >> $2
+echo Coverage uniformity \(10X\)\	กร 90% >> $2
